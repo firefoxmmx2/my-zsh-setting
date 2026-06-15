b06a3ce329b0d3c0694f513faae4f268c4aeb0e0
huxin <huxin@aisino.com>
2026-06-15 17:15:53 +0800

    npm 配置更新。

--- npm.zsh
+++ npm.zsh
@@ -2,3 +2,16 @@
 
 PATH=$PATH:$HOME/.local/bin
 export NODE_OPTIONS="--max-old-space-size=2000 --max-http-header-size=10000000"
+
+# pnpm
+# tabtab source for packages
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+
+# pnpm
+export PNPM_HOME="${HOME}/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME/bin:"*) ;;
+  *) export PATH="$PNPM_HOME/bin:$PATH" ;;
+esac
+# pnpm end
